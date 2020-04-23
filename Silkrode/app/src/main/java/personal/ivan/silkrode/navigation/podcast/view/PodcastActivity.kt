7f25a8f3c9854b34fc7fa2e68a0ba2ee9a78b517
Ivan Kuo <ivanclair0710@gmail.com>
2020-04-23 15:09:38 +0800

    story-3 create navigation graph

---
+++ Silkrode/app/src/main/java/personal/ivan/silkrode/navigation/podcast/view/PodcastActivity.kt
@@ -0,0 +1,11 @@
+package personal.ivan.silkrode.navigation.podcast.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class PodcastActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
