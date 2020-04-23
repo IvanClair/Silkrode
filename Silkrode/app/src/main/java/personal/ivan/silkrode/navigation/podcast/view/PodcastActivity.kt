038da596f5bc4f1a546b3720d9b8502fff3d97a7
Ivan Kuo <ivanclair0710@gmail.com>
2020-04-23 16:18:04 +0800

    story-3 create fragments

--- Silkrode/app/src/main/java/personal/ivan/silkrode/navigation/podcast/view/PodcastActivity.kt
+++ Silkrode/app/src/main/java/personal/ivan/silkrode/navigation/podcast/view/PodcastActivity.kt
@@ -2,10 +2,19 @@ package personal.ivan.silkrode.navigation.podcast.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import personal.ivan.silkrode.databinding.ActivityPodcastBinding
 
 class PodcastActivity : AppCompatActivity() {
 
+    // View Binding
+    private val mBinding: ActivityPodcastBinding by lazy {
+        ActivityPodcastBinding.inflate(layoutInflater)
+    }
+
+    /* ------------------------------ Life Cycle */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(mBinding.root)
     }
 }
