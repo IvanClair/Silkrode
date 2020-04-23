29a3bdd4ba1da8f0fd76a50181774e4026d53a37
Ivan Kuo <ivanclair0710@gmail.com>
2020-04-23 19:51:02 +0800

    story-5 create API response model

---
+++ Silkrode/app/src/test/java/personal/ivan/silkrode/DateFormatTest.kt
@@ -0,0 +1,34 @@
+package personal.ivan.silkrode
+
+import org.junit.Assert
+import org.junit.Test
+import personal.ivan.silkrode.api.Collection
+import personal.ivan.silkrode.api.ContentFeed
+import personal.ivan.silkrode.util.DateFormatUtil
+
+class DateFormatTest {
+
+    @Test
+    fun formatCollectionReleaseDate_isCorrect() {
+        val expected = "09 Mar 2020"
+        val actual =
+            DateFormatUtil().formatDateString(
+                input = "2020-03-09T19:54:00Z",
+                inputPattern = Collection.DATE_PATTERN,
+                outputPattern = "dd MMM yyyy"
+            )
+        Assert.assertEquals(expected, actual)
+    }
+
+    @Test
+    fun formatContentFeedPublishedDate_isCorrect() {
+        val expected = "Mon, 09 Mar 2020"
+        val actual =
+            DateFormatUtil().formatDateString(
+                input = "Mon, 09 Mar 2020 15:01:16 +0000",
+                inputPattern = ContentFeed.DATE_PATTERN,
+                outputPattern = "EEE, dd MMM yyyy"
+            )
+        Assert.assertEquals(expected, actual)
+    }
+}
