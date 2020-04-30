45b25e278398ad2295056fadd729f90ff889072e
Ivan Kuo <ivanclair0710@gmail.com>
2020-04-30 14:25:52 +0800

    story-11 remove unused import

--- Silkrode/app/src/main/java/personal/ivan/silkrode/navigation/podcast/view/PodcastActivity.kt
+++ Silkrode/app/src/main/java/personal/ivan/silkrode/navigation/podcast/view/PodcastActivity.kt
@@ -1,17 +1,10 @@
 package personal.ivan.silkrode.navigation.podcast.view
 
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.widget.SeekBar
 import androidx.activity.viewModels
-import androidx.lifecycle.Observer
 import dagger.android.support.DaggerAppCompatActivity
-import personal.ivan.silkrode.R
 import personal.ivan.silkrode.databinding.ActivityPodcastBinding
-import personal.ivan.silkrode.databinding.FragmentPlayBinding
 import personal.ivan.silkrode.di.AppViewModelFactory
-import personal.ivan.silkrode.extension.enableOrDisable
-import personal.ivan.silkrode.extension.setTintForPlayerStatus
 import personal.ivan.silkrode.navigation.podcast.viewmodel.PodcastViewModel
 import personal.ivan.silkrode.util.DateFormatUtil
 import javax.inject.Inject
